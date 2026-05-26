720a490494de29852f5072d4fb5d5d030ccedafe
LeAndre <187464993+lecanact@users.noreply.github.com>
2026-05-25 20:33:55 -0400

    Downgrade Elixir version requirement to 1.16

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Lanyard.MixProject do
     [
       app: :lanyard,
       version: "0.1.0",
-      elixir: "~> 1.19",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
